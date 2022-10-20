4dc1822bb74381276e84caee6747b775e22b291b
akhil <akhilsails@gmail.com>
2022-10-20 13:22:21 +0530

    working stage-1 code

--- repository/repository.go
+++ repository/repository.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"errors"
-
 	"go-gin-CRUD/domain"
 )
 
@@ -21,52 +19,4 @@ func NewRepository() *Repository {
 	return &Repository{
 		DB: map[string]domain.Book{example.ID: example},
 	}
-}
-
-func (repo *Repository) GetAllBooks() ([]domain.Book, error) {
-	var books []domain.Book
-
-	for _, book := range repo.DB {
-		books = append(books, book)
-	}
-
-	return books, nil
-}
-
-func (repo *Repository) GetBook(id string) (domain.Book, error) {
-	if book, exist := repo.DB[id]; exist {
-		return book, nil
-	}
-
-	return domain.Book{}, errors.New("not found")
-}
-
-func (repo *Repository) CreateBook(book domain.Book) (domain.Book, error) {
-	if _, exist := repo.DB[book.ID]; exist {
-		return domain.Book{}, errors.New("already exist")
-	}
-
-	repo.DB[book.ID] = book
-
-	return book, nil
-}
-
-func (repo *Repository) UpdateBook(book domain.Book) error {
-	if _, exist := repo.DB[book.ID]; !exist {
-		return errors.New("not found")
-	}
-
-	repo.DB[book.ID] = book
-
-	return nil
-}
-
-func (repo *Repository) DeleteBook(id string) error {
-	if _, exist := repo.DB[id]; !exist {
-		return errors.New("not found")
-	}
-
-	delete(repo.DB, id)
-
-	return nil
 }
--- services/services.go
+++ services/services.go
@@ -1,10 +1,7 @@
 package services
 
 import (
-	"go-gin-CRUD/domain"
 	"go-gin-CRUD/repository"
-
-	"github.com/google/uuid"
 )
 
 type Services struct {
@@ -15,39 +12,4 @@ func NewServices() *Services {
 	return &Services{
 		Repository: repository.NewRepository(),
 	}
-}
-
-func (s *Services) GetAllBooks() ([]domain.Book, error) {
-	return s.Repository.GetAllBooks()
-}
-
-func (s *Services) GetBook(id string) (domain.Book, error) {
-	return s.Repository.GetBook(id)
-}
-
-func (s *Services) CreateBook(book domain.Book) (domain.Book, int, error) {
-	book.ID = uuid.New().String()
-
-	book, err := s.Repository.CreateBook(book)
-	if err != nil {
-		return domain.Book{}, 400, err
-	}
-
-	return book, 201, nil
-}
-
-func (s *Services) UpdateBook(book domain.Book) (domain.Book, error) {
-	if err := s.Repository.UpdateBook(book); err != nil {
-		return domain.Book{}, err
-	}
-
-	return book, nil
-}
-
-func (s *Services) DeleteBook(id string) error {
-	if err := s.Repository.DeleteBook(id); err != nil {
-		return err
-	}
-
-	return nil
 }
